50878982b7d4367db7e4d8b4f2657c2e5ee2e63b
Mani-dev8 <manishsewamahto090802@gmail.com>
2025-12-06 16:33:31 +0530

    feat: handled the next config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack:{
+    root:'/Users/manish/Desktop/manish/my-app'
+  }
 };
 
 export default nextConfig;
