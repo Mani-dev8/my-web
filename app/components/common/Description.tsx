9dae129ade08bce975566facc0543b9c8312f230
Mani-dev8 <manishsewamahto090802@gmail.com>
2025-12-06 16:24:14 +0530

    feat: handled the heading resonsivemess

--- app/components/common/Description.tsx
+++ app/components/common/Description.tsx
@@ -10,7 +10,7 @@ function Description({ text, className }: Props) {
   return (
     <p
       className={cn(
-        `text-zinc-500 text-sm max-w-96 lg:text-base lg:max-w-160 md:text-start text-center leading-tight ${className}`
+        `text-zinc-500 text-sm max-w-96 lg:text-base lg:max-w-160 lg:text-start text-center leading-tight ${className}`
       )}
     >
       {text}
--- app/components/common/Heading.tsx
+++ app/components/common/Heading.tsx
@@ -7,7 +7,7 @@ type Props = PropsWithChildren & {
 
 function Heading({ children, className }: Props) {
     return (
-        <h2 className={cn(`text-[28px] md:text-3xl lg:text-5xl font-medium text-gray-900 mb-4 tracking-tight leading-tight text-center lg:text-start ${className}`)}>
+        <h2 className={cn(`text-[28px] md:text-3xl lg:text-5xl xl:text-6xl md:font-medium lg:font-normal text-gray-900 mb-4 tracking-tight leading-tight text-center lg:text-start lg:max-w-2xl xl:max-w-3xl ${className}`)}>
             {children}
         </h2>
     )
